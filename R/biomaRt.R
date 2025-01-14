ad5c6c62abdff7407cd58b386f9621f9435b389e
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2025-01-14 11:15:43 +0000

    R

---
+++ R/biomaRt.R
@@ -0,0 +1,21 @@
+#' @title biomaRt
+#' @description Curation of biomaRt
+#' @docType data
+#' @keywords datasets internal
+#' @format A data frame with 42198 rows and 11 variables:
+#' \describe{
+#'   \item{\code{ensembl_gene_id}}{ENSEMBL gene id}
+#'   \item{\code{chromosome_name}}{Chromosome name (1-22,X,Y)}
+#'   \item{\code{start_position}}{start}
+#'   \item{\code{end_position}}{end}
+#'   \item{\code{description}}{Description}
+#'   \item{\code{hgnc_symbol}}{HGNC symbol}
+#'   \item{\code{ensembl_transcript_id}}{ENSEMBL transcript id}
+#'   \item{\code{transcription_start_site}}{TSS}
+#'   \item{\code{transcript_start}}{Transcript start}
+#'   \item{\code{transcript_end}}{Transcript end}
+#'   \item{\code{uniprotswissprot}}{UnitProt id}
+#' }
+#' @details extraction using R.
+
+"biomaRt"
---
+++ R/gaawr2.R
@@ -0,0 +1,40 @@
+#' Genetic Association Analysis with R (II)
+#'
+#' @docType package
+#' @name gaawr2
+#' @aliases gaawr2-package
+#'
+#' @details
+#' Available data are listed in the following table.
+#'
+#' Objects                   |  Description
+#' --------------------------|------------------------------------
+#' **Datasets**              |   &nbsp;
+#' [`biomaRt`]               |   Curated data from biomaRt
+#'
+#' Some generic description for the datasets are as follows.
+#' -  chr Chromosome.
+#' -  start Start position.
+#' -  end End position.
+#' -  gene Gene name.
+#' -  UniProt UniProt ID.
+#'
+#' @section Usage:
+#' Vignettes on package usage:
+#' - An Overview of pQTLdata. `vignette("gaawr2")`.
+#' @md
+#'
+#' @import knitr
+#' @importFrom Rdpack reprompt
+#'
+#' @author Jing Hua Zhao in collaboration with other colleagues.
+#' @keywords internal
+#' @examples
+#' \dontrun{
+#' # See more on SCALLOP-INF at https://jinghuazhao.github.io/INF/
+#' # datasets
+#' head(biomaRt)
+#'
+#' }
+
+"_PACKAGE"
