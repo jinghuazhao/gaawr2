7c46452934e6133fe24e8b1aa7fe08e87860e2cb
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2025-01-14 13:29:31 +0000

    inst

--- inst/scripts/docs.sh
+++ inst/scripts/docs.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/bash
 
+function setup()
+{
+Rscript -e '
+  suppressMessages(library(pkgdown))
+  {
+    knitr::knit("README.Rmd")
+    roxygen2::roxygenise()
+    pkgdown::build_site()
+  }
+  pkgdown::build_site_github_pages()
+  usethis::use_github_action("pkgdown", save_as = "R-CMD-check.yaml", ref = NULL, ignore = TRUE, open = FALSE)
+# clean_site(); init_site(); build_home(); build_news(); build_articles(); build_reference(); build_search()
+'
+# devtools::build_rmd() is equivalent but limited, so knitr::knit/pandoc are better options.
+# pandoc README.md --citeproc --mathjax -s --self-contained -o index.html
+}
+
+setup
+
 for f in $(ls)
 do
   echo adding ${f}
