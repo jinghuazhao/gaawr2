e36e9d3aa5901a48f93e23805e6e5b27a88c5867
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2025-01-16 19:15:28 +0000

    inst

---
+++ inst/scripts/OpenTargets.R
@@ -0,0 +1,33 @@
+library(httr)
+library(jsonlite)
+
+gene_id <- "ENSG00000164308"
+query_string = "
+  query target($ensemblId: String!){
+    target(ensemblId: $ensemblId){
+      id
+      approvedSymbol
+      biotype
+      geneticConstraint {
+        constraintType
+        exp
+        obs
+        score
+        oe
+        oeLower
+        oeUpper
+      }
+      tractability {
+        label
+        modality
+        value
+      }
+    }
+  }
+"
+base_url <- "https://api.platform.opentargets.org/api/v4/graphql"
+variables <- list("ensemblId" = gene_id)
+post_body <- list(query = query_string, variables = variables)
+r <- httr::POST(url=base_url, body=post_body, encode='json')
+data <- iconv(r, "latin1", "ASCII")
+content <- jsonlite::fromJSON(data)
