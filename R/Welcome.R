ba10cfa6e49d4cbba493f7c016fd9afd6c0607ba
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2025-01-22 17:53:25 +0000

    R

---
+++ R/Welcome.R
@@ -0,0 +1,14 @@
+#' @title FUNCTION_TITLE
+#' @description A hello, world extension.
+#' @param n a counter.
+#'
+#' @details
+#' It simply says times in the message.
+#' @export
+#' @return a message.
+#' @keywords internal
+
+Welcome <- function(n)
+{
+   cat("Welcome to gaawr2", n, "times", "\n")
+}
