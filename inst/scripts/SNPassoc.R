21bdd41ca4d0d7141fc51ea633724c4fda07956d
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2025-02-03 17:48:50 +0000

    SNPassoc.R

--- inst/scripts/SNPassoc.R
+++ inst/scripts/SNPassoc.R
@@ -1,27 +1,38 @@
 options(width=200,digits=2)
-library(dplyr)
-library(SNPassoc)
 data(asthma, package = "SNPassoc")
-str(asthma, list.len=9)
+str(asthma, list.len=8)
 knitr::kable(asthma[1:3,1:8],caption="First three records & two SNPs")
-
-snpCols <- grep("^rs", colnames(asthma), value=TRUE)[1:5]
-snps <- setupSNP(data=asthma[snpCols], colSNPs=1:length(snpCols), sep="")
+snpCols <- colnames(asthma)[6+(1:2)]
+snps <- SNPassoc::setupSNP(data=asthma[snpCols], colSNPs=1:length(snpCols), sep="")
+head(snps)
 summary(snps, print=FALSE)
 lapply(snps, head)
 lapply(snps, summary)
-knitr::kable(tableHWE(snps),caption="Table of HWE")
+SNPassoc::tableHWE(snps)
 
-asthma.snps <- setupSNP(data=asthma, colSNPs=grep("^rs", colnames(asthma)), sep="") %>%
+asthma.snps <- asthma %>%
                dplyr::rename(cc=casecontrol) %>%
-               dplyr::select(1:11)
-association(bmi ~ rs4490198, asthma.snps)
-association(cc ~ rs4490198, data=asthma.snps)
-association(cc ~ rs4490198 + country + smoke, asthma.snps)
-association(cc ~ rs4490198 + survival::strata(gender), asthma.snps)
-association(cc ~ rs4490198, asthma.snps, subset=country=="Spain")
-association(cc ~ dominant(rs4490198)*factor(smoke), data=asthma.snps)
-association(cc ~ rs4490198*factor(rs11123242), data=asthma.snps, model.interaction = "dominant" )
+               SNPassoc::setupSNP(colSNPs=(6+1):ncol(.), sep="")
+# Model 1: Simple SNP association with BMI
+SNPassoc::association(bmi ~ rs4490198, data = asthma.snps)
+
+# Model 2: SNP association with case-control status
+SNPassoc::association(cc ~ rs4490198, data = asthma.snps)
+
+# Model 3: SNP association with covariates (country and smoke)
+SNPassoc::association(cc ~ rs4490198 + country + smoke, data = asthma.snps)
+
+# Model 4: SNP association with stratification by gender
+SNPassoc::association(cc ~ rs4490198 + survival::strata(gender), data = asthma.snps)
+
+# Model 5: SNP association with subset (only Spain)
+SNPassoc::association(cc ~ rs4490198, data = asthma.snps, subset = country == "Spain")
+
+# Model 6: Interaction between SNP (dominant model) and smoking
+SNPassoc::association(cc ~ SNPassoc::dominant(rs4490198) * factor(smoke), data = asthma.snps)
+
+# Model 7: Interaction between two SNPs (dominant model for rs4490198)
+SNPassoc::association(cc ~ rs4490198 * factor(rs11123242), data = asthma.snps, model.interaction = "dominant")
 
 assoc <- SNPassoc::WGassociation(cc, data=asthma.snps, mc.cores=5)
 assoc %>%
