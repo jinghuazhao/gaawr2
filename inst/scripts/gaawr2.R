2c3a0b03a30b028b58ccfc33eae58b8bcf6afbfe
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2025-03-14 08:18:05 +0000

    inst

--- inst/scripts/gaawr2.R
+++ inst/scripts/gaawr2.R
@@ -4,4 +4,10 @@ create_gaawr2 <- function()
   create_package("gaawr2")
   use_mit_license()
   use_github_action("pkgdown", save_as = "R-CMD-check.yaml", ref = NULL, ignore = TRUE, open = FALSE)
+  use_logo("pkgdown/logo.png")
+# Image in README.Rmd
+# <img src="man/figures/logo.png" align="right" height="86" alt="gaawr2 website" />
+  library(pkgdown)
+# favicons
+  build_favicons(overwrite=TRUE)
 }
