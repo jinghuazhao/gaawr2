72118768acaaeb5b0a3b207c63af466e37e855df
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2025-03-24 12:59:07 +0000

    inst

--- inst/csl/url.R
+++ inst/csl/url.R
@@ -6,20 +6,20 @@ library(bibtex)
 library(httr)
 library(XML)
 
-# Function to check URL status
+# to check URL status
 check_url_status <- function(url) {
-  response <- tryCatch(HEAD(url), error = function(e) NULL)
-  if (!is.null(response) && status_code(response) == 200) {
+  response <- tryCatch(httr::HEAD(url), error = function(e) NULL)
+  if (!is.null(response) && httr::status_code(response) == 200) {
     return(TRUE)
   } else {
     return(FALSE)
   }
 }
 
-# Function to check URL status in a BibTeX file
+# to check URL status in a BibTeX file
 check_bib_url_status <- function(bib_file) {
-  bib <- read.bib(bib_file)
-  for (i in 1:length(bib)) {
+  bib <- bibtex::read.bib(bib_file)
+  for (i in seq_along(bib)) {
     if ("url" %in% names(bib[[i]])) {
       url <- bib[[i]]$url
       if (!check_url_status(url)) {
@@ -30,21 +30,19 @@ check_bib_url_status <- function(bib_file) {
   return(bib)
 }
 
-# Function to check and replace URLs in the CSL file
+# to check and replace URLs in the CSL file
 namespace <- c(csl = "http://purl.org/net/xbiblio/csl")
 check_csl_urls <- function(csl_file_path) {
-  csl_doc <- xmlParse(csl_file_path)
-  link_nodes <- xpathSApply(csl_doc, "//csl:link", xmlGetAttr, "href", namespaces = namespace)
+  csl_doc <- XML::xmlParse(csl_file_path)
+  link_nodes <- XML::xpathSApply(csl_doc, "//csl:link", XML::xmlGetAttr, "href", namespaces = namespace)
   for (i in seq_along(link_nodes)) {
-    url <- link_nodes[i]
-    if (!check_url_status(url)) {
-      link_nodes[i] <- "URL not available"
+    href <- link_nodes[i]
+    if (!check_url_status(href)) {
+      xpath_query <- paste("//csl:link[position()=", i, "]", sep = "")
+      XML::xpathSApply(csl_doc, xpath_query, function(node) {
+        XML::xmlAttrs(node)["href"] <- "URL not available"
+      },namespaces = namespace)
     }
-  }
-  for (i in 1:length(link_nodes)) {
-    xpathApply(csl_doc, sprintf("//link[@href='%s']", link_nodes[i]), function(node) {
-      xmlAttrs(node)["href"] <- link_nodes[i]
-    })
   }
   csl_doc
 }
@@ -53,11 +51,10 @@ test_url <- function()
 {
 # Check URLs in the .bib file
   bib_file <- system.file("REFERENCES.bib",package="gaawr2")
-  updated_bib <- check_bib_url_status(bib_file)
-  write.bib(updated_bib, file = "REFERENCES_updated.bib")
+  bib_updated <- check_bib_url_status(bib_file)
+  bibtex::write.bib(bib_updated, file = "REFERENCES_updated.bib")
 
 # Run URL check for CSL file
   csl_file <- system.file("csl","nature-genetics.csl",package="gaawr2")
-  csl <- "nature-genetics.csl"
   check_csl_urls(csl_file)
 }
