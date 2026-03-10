7292b2b8e8bce4d14e4ecce1fac8b68ec549e9bb
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2026-03-10 13:33:09 +0000

    inst

--- inst/scripts/cran.sh
+++ inst/scripts/cran.sh
@@ -34,8 +34,25 @@ module load gcc/11.3.0/gcc/4zpip55j
 echo $R_LIBS
 
 cd ~/R
-export version=$(awk '/Version/{print $2}' $src/DESCRIPTION)
-echo "Now, build, install and check gaawr2 $version"
-R CMD build --compact-vignettes=both --md5 --resave-data --log gaawr2
-R CMD INSTALL --compact-docs --data-compress=xz gaawr2_${version}.tar.gz
-R CMD check --as-cran gaawr2_${version}.tar.gz
+export version=$(awk -F': *' '/^Version:/{print $2}' "$src/DESCRIPTION")
+pkg="gaawr2"
+
+echo "Step 1: Build package with vignettes"
+R CMD build --compact-vignettes=both "$dst"
+
+echo "Step 2: Install package to generate doc/"
+R CMD INSTALL --build ${pkg}_${version}.tar.gz
+
+echo "Step 3: Copy built vignettes into inst/doc"
+mkdir -p "$dst/inst/doc"
+cp -r ~/R/${pkg}/doc/* "$dst/inst/doc/" 2>/dev/null || true
+
+echo "Step 4: Remove vignette sources for CRAN version"
+rm -rf "$dst/vignettes"
+sed -i '/^VignetteBuilder:/d' "$dst/DESCRIPTION"
+
+echo "Step 5: Build CRAN submission tarball"
+R CMD build --md5 --resave-data "$dst"
+
+echo "Step 6: Check package"
+R CMD check --as-cran ${pkg}_${version}.tar.gz
